089767e7bdadcafe45181958e8309b69a19d07d3
Leonardo Negri <leonardo.negri@helsinki.fi>
2026-06-25 16:12:33 +0200

    new command for snakemake

---
+++ jobs/run_simulation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# set how many runs here. This can be improved in the future but for now it works
+num_runs=1
+# change into dime folder
+folder="$(dirname "$1")"
+file="$(basename "$1")"
+cd "$folder"
+gfortran "$file" -o a.out >/dev/null 2>&1 || { echo "Compilation failed"; exit 1; }
+# save output to log.out
+./a.out "$num_runs" > log.out 2>&1 || { echo "Run failed, see log.out"; exit 1; }
