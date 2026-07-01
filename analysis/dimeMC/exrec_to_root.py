eb00c8b241f3208c7ff14f92be2be96593c811a0
Leonardo Negri <leonardo.negri@helsinki.fi>
2026-07-01 14:17:55 +0200

    change branch creation from vector to scalars for easier access in the code

--- analysis/dimeMC/exrec_to_root.py
+++ analysis/dimeMC/exrec_to_root.py
@@ -4,6 +4,7 @@ import ROOT
 import sys
 import numpy as np
 
+from array import array
 from pathlib import Path
 
 
@@ -25,6 +26,7 @@ def read_file(input_file, output_file):
     ntrk_list = []       # number of produced particles
     mass_loss_list = []  # M = sqrt(xi_1 * xi_2 * s)
     produced_particles = []  # list of [event_num, id, px, py, pz, e] for each produced particle
+    invariant_mass_list = []  # invariant mass of the system of produced particles
     
     # Read exrec.dat file
     with open(input_file, 'r') as f:
@@ -113,6 +115,8 @@ def read_file(input_file, output_file):
                 event_numbers.append(current_event)
 
                 # Read produced particles (px, py, pz, e)
+                particle_vec = ROOT.Math.PxPyPzEVector(0, 0, 0, 0)
+
                 for _ in range(ntrk):
                     particle_line = lines[line_idx].strip()
                     particle_parts = particle_line.split()
@@ -126,8 +130,15 @@ def read_file(input_file, output_file):
                     
                     # Store produced particle data with event number and id
                     produced_particles.append([current_event, particle_id, px, py, pz, e, m])
-                    
+
+                    if abs(particle_id) == 211:
+                        temp_vec = ROOT.Math.PxPyPzEVector(px, py, pz, e)
+                        particle_vec = particle_vec + temp_vec
+
                     line_idx += 1
+                
+                invariant_mass_list.append(particle_vec.M())
+
             except (ValueError, IndexError):
                 # In case of formatting problems skip line
                 line_idx += 1
@@ -140,93 +151,80 @@ def read_file(input_file, output_file):
     tree = ROOT.TTree("particles", "Particle data from exrec.dat")
     
     # Create branches for event-level data
-    event_no = ROOT.std.vector('int')()
-    incoming_p1_pz = ROOT.std.vector('double')()
-    incoming_p1_e = ROOT.std.vector('double')()
-    incoming_p2_pz = ROOT.std.vector('double')()
-    incoming_p2_e = ROOT.std.vector('double')()
-    outgoing_p1_px = ROOT.std.vector('double')()
-    outgoing_p1_py = ROOT.std.vector('double')()
-    outgoing_p1_pz = ROOT.std.vector('double')()
-    outgoing_p1_e = ROOT.std.vector('double')()
-    outgoing_p2_px = ROOT.std.vector('double')()
-    outgoing_p2_py = ROOT.std.vector('double')()
-    outgoing_p2_pz = ROOT.std.vector('double')()
-    outgoing_p2_e = ROOT.std.vector('double')()
-    ntrk = ROOT.std.vector('int')()
-    mass_loss_p = ROOT.std.vector('double')()
+    event_no = array('i', [0])
+    incoming_p1_pz = array('d', [0.0])
+    incoming_p1_e = array('d', [0.0])
+    incoming_p2_pz = array('d', [0.0])
+    incoming_p2_e = array('d', [0.0])
+    outgoing_p1_px = array('d', [0.0])
+    outgoing_p1_py = array('d', [0.0])
+    outgoing_p1_pz = array('d', [0.0])
+    outgoing_p1_e = array('d', [0.0])
+    outgoing_p2_px = array('d', [0.0])
+    outgoing_p2_py = array('d', [0.0])
+    outgoing_p2_pz = array('d', [0.0])
+    outgoing_p2_e = array('d', [0.0])
+    ntrk = array('i', [0])
+    mass_loss_p = array('d', [0.0])
     produced_id = ROOT.std.vector('int')()
     produced_px = ROOT.std.vector('double')()
     produced_py = ROOT.std.vector('double')()
     produced_pz = ROOT.std.vector('double')()
     produced_e = ROOT.std.vector('double')()
     produced_m = ROOT.std.vector('double')()
+    invariant_mass = array('d', [0.0])
     
-    tree.Branch("event_number", event_no)
-    tree.Branch("p1_in_pz", incoming_p1_pz)
-    tree.Branch("p1_in_e", incoming_p1_e)
-    tree.Branch("p2_in_pz", incoming_p2_pz)
-    tree.Branch("p2_in_e", incoming_p2_e)
-    tree.Branch("p1_out_px", outgoing_p1_px)
-    tree.Branch("p1_out_py", outgoing_p1_py)
-    tree.Branch("p1_out_pz", outgoing_p1_pz)
-    tree.Branch("p1_out_e", outgoing_p1_e)
-    tree.Branch("p2_out_px", outgoing_p2_px)
-    tree.Branch("p2_out_py", outgoing_p2_py)
-    tree.Branch("p2_out_pz", outgoing_p2_pz)
-    tree.Branch("p2_out_e", outgoing_p2_e)
-    tree.Branch("ntrk", ntrk)
-    tree.Branch("mass_loss_p", mass_loss_p)
+    tree.Branch("event_number", event_no, "event_number/I")
+    tree.Branch("p1_in_pz", incoming_p1_pz, "p1_in_pz/D")
+    tree.Branch("p1_in_e", incoming_p1_e, "p1_in_e/D")
+    tree.Branch("p2_in_pz", incoming_p2_pz, "p2_in_pz/D")
+    tree.Branch("p2_in_e", incoming_p2_e, "p2_in_e/D")
+    tree.Branch("p1_out_px", outgoing_p1_px, "p1_out_px/D")
+    tree.Branch("p1_out_py", outgoing_p1_py, "p1_out_py/D")
+    tree.Branch("p1_out_pz", outgoing_p1_pz, "p1_out_pz/D")
+    tree.Branch("p1_out_e", outgoing_p1_e, "p1_out_e/D")
+    tree.Branch("p2_out_px", outgoing_p2_px, "p2_out_px/D")
+    tree.Branch("p2_out_py", outgoing_p2_py, "p2_out_py/D")
+    tree.Branch("p2_out_pz", outgoing_p2_pz, "p2_out_pz/D")
+    tree.Branch("p2_out_e", outgoing_p2_e, "p2_out_e/D")
+    tree.Branch("ntrk", ntrk, "ntrk/I")
+    tree.Branch("mass_loss_p", mass_loss_p, "mass_loss_p/D")
     tree.Branch("produced_id", produced_id)
     tree.Branch("produced_px", produced_px)
     tree.Branch("produced_py", produced_py)
     tree.Branch("produced_pz", produced_pz)
     tree.Branch("produced_e", produced_e)
     tree.Branch("produced_m", produced_m)
+    tree.Branch("inv_mass", invariant_mass, "inv_mass/D")
     
     # Fill tree with event data
     for i in range(len(event_numbers)):
-        event_no.clear()
-        incoming_p1_pz.clear()
-        incoming_p1_e.clear()
-        incoming_p2_pz.clear()
-        incoming_p2_e.clear()
-        outgoing_p1_px.clear()
-        outgoing_p1_py.clear()
-        outgoing_p1_pz.clear()
-        outgoing_p1_e.clear()
-        outgoing_p2_px.clear()
-        outgoing_p2_py.clear()
-        outgoing_p2_pz.clear()
-        outgoing_p2_e.clear()
-        ntrk.clear()
-        mass_loss_p.clear()
         produced_id.clear()
         produced_px.clear()
         produced_py.clear()
         produced_pz.clear()
         produced_e.clear()
         produced_m.clear()
-        
-        event_no.push_back(event_numbers[i])
-        
-        incoming_p1_pz.push_back(p1_in_pz_list[i])
-        incoming_p1_e.push_back(p1_in_e_list[i])
-        incoming_p2_pz.push_back(p2_in_pz_list[i])
-        incoming_p2_e.push_back(p2_in_e_list[i])
 
-        outgoing_p1_px.push_back(p1_out_px_list[i])
-        outgoing_p1_py.push_back(p1_out_py_list[i])
-        outgoing_p1_pz.push_back(p1_out_pz_list[i])
-        outgoing_p1_e.push_back(p1_out_e_list[i])
+        event_no[0] = event_numbers[i]
+        incoming_p1_pz[0] = p1_in_pz_list[i]
+        incoming_p1_e[0] = p1_in_e_list[i]
+        incoming_p2_pz[0] = p2_in_pz_list[i]
+        incoming_p2_e[0] = p2_in_e_list[i]
 
-        outgoing_p2_px.push_back(p2_out_px_list[i])
-        outgoing_p2_py.push_back(p2_out_py_list[i])
-        outgoing_p2_pz.push_back(p2_out_pz_list[i])
-        outgoing_p2_e.push_back(p2_out_e_list[i])
+        outgoing_p1_px[0] = p1_out_px_list[i]
+        outgoing_p1_py[0] = p1_out_py_list[i]
+        outgoing_p1_pz[0] = p1_out_pz_list[i]
+        outgoing_p1_e[0] = p1_out_e_list[i]
 
-        ntrk.push_back(ntrk_list[i])
-        mass_loss_p.push_back(mass_loss_list[i])
+        outgoing_p2_px[0] = p2_out_px_list[i]
+        outgoing_p2_py[0] = p2_out_py_list[i]
+        outgoing_p2_pz[0] = p2_out_pz_list[i]
+        outgoing_p2_e[0] = p2_out_e_list[i]
+
+        ntrk[0] = ntrk_list[i]
+        mass_loss_p[0] = mass_loss_list[i]
+        invariant_mass[0] = invariant_mass_list[i]
         
         # Add produced particles for this event
         for particle in produced_particles:
