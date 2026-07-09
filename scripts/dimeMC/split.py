b0d24002e290595e68483b8782c6abdf70574b97
Leonardo Negri <leonardo.negri@helsinki.fi>
2026-07-09 10:36:43 +0200

    create split function to categorize dime between P and D events

---
+++ scripts/dimeMC/split.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import ROOT
+import sys
+import math
+
+from pathlib import Path
+import numpy as np
+
+
+def split_file(dime_file, save_path):
+    df = ROOT.RDataFrame("particles", str(dime_file))
+
+    save_path = save_path / f'{dime_file.stem.replace}_D.root'
+
+    # Find the quadrant of the angle and get the difference between them
+    df.Define("q_diff",
+            "TLorentzVector p1(p1_out_px, p1_out_py, p1_out_pz, p1_out_e);"
+            "TLorentzVector p2(p2_out_px, p2_out_py, p2_out_pz, p2_out_e);"
+            "int q_1 = int( p1.Phi()/ (M_pi/2) );"
+            "int q_2 = int( p2.Phi()/ (M_pi/2) );"
+            "return abs(q1 - q2);")
+    
+    # Diagonal events happen when the quadrants differ by 2. Parallel otherwise.
+    df_D = df.Filter("q_diff == 2")
+    df_P = df.Filter("q_diff != 2")
+
+    df_D.Snapshot("particles", str(save_path).replace("_A", "_D"))
+    df_P.Snapshot("particles", str(save_path).replace("_A", "_D"))
+
+def main():
+    data_file = Path(sys.argv[1])
+
+    save_path = Path("data/dimeMC")
+    save_path.mkdir(parents=True, exist_ok=True)
+
+    split_file(data_file, save_path)
+
+
+
+if __name__ == '__main__':
+    main()
