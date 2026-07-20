c1adfa76888357b80ced38fb00895c5517bbb477
Leonardo Negri <leonardo.negri@helsinki.fi>
2026-07-20 14:16:01 +0200

    refactor simulation code to take the number of runs from commandline and rename file after completion

--- scripts/jobs/run_simulation.sh
+++ scripts/jobs/run_simulation.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
-
+ 
 # set how many runs here. This can be improved in the future but for now it works
-num_runs=50000
+num_runs=500
+ 
+# $1 = path to the fortran source file
+# $2 = production rho/phi. Ignored by resonant file
+fortran_path="$1"
+num_runs="$2"
+production="$3"
+ 
 # change into dime folder
-folder="$(dirname "$1")"
-file="$(basename "$1")"
+folder="$(dirname "$fortran_path")"
+file="$(basename "$fortran_path")"
 cd "$folder"
+ 
 gfortran "$file" -o a.out >/dev/null 2>&1 || { echo "Compilation failed"; exit 1; }
+ 
 # save output to log.out
-./a.out "$num_runs" > log.out 2>&1 || { echo "Run failed, see log.out"; exit 1; }
+./a.out "$num_runs" "$production" > log.out 2>&1 || { echo "Run failed, see log.out"; exit 1; }
+ 
+# rename exrec.dat only if the argument for production was given
+if [ -n "$production" ]; then
+    mv exrec.dat "${production}_exrec.dat"
+fi
