f90cada377d5644c7d1fc84e6e335a981ab4e0d1
Leonardo Negri <leonardo.negri@helsinki.fi>
2026-07-21 12:31:48 +0200

    fix issue with colours not rendering correctly

--- scripts/utilities/plotter.py
+++ scripts/utilities/plotter.py
@@ -10,31 +10,32 @@ def plot_joint(data_hist, resonant_hist, nonreson_hists, save_path, title):
         if max_val != 0:
             h.Scale(1.0 / max_val)
 
-    ROOT.gStyle.SetPalette(ROOT.kRainbow)
-
     data_hist.SetLineWidth(3)
+    data_hist.SetLineStyle(1)
     data_hist.SetTitle(title)
 
     resonant_hist.SetLineWidth(3)
-    resonant_hist.SetLineStyle(2)
+    resonant_hist.SetLineStyle(7)
 
     for hist in nonreson_list:
         hist.SetLineWidth(3)
-        hist.SetLineStyle(3)
+        hist.SetLineStyle(2)
 
     ymax = max(h.GetMaximum() for h in (data_hist, resonant_hist, *nonreson_list))
     data_hist.SetMaximum(ymax * 1.1)
 
     c = ROOT.TCanvas("c", "c", 1600, 1200)
+    
+    ROOT.gStyle.SetPalette(ROOT.kBlueRedYellow)
+    data_hist.Draw("HIST")
     for hist in nonreson_list:
         hist.Draw("HIST PLC SAME")
     resonant_hist.Draw("HIST PLC SAME")
-    data_hist.Draw("HIST PLC SAME")
 
     legend = ROOT.TLegend(0.7, 0.7, 0.9, 0.9)
     legend.AddEntry(data_hist, "Data", "l")
     legend.AddEntry(resonant_hist, "DimeMC Resonant", "l")
-    for key in nonreson_hists:
+    for key, hist in zip(nonreson_hists, nonreson_list):
         legend.AddEntry(hist, f"DimeMC {key}", "l")
     legend.Draw()
 
